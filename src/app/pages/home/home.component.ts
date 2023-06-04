06e7a921f93f6a6e8098c17a7fd54179f6ce0252
Lakshya Raj Chandra <lakshyarchandra@gmail.com>
2023-06-04 10:24:46 +0530

    Environment Created and Api File Added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-home',
@@ -8,55 +9,19 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   public productDetails: any;
+  public  products: any;
 
   public getProductDetail(product:any){
     console.log(product);
 
     this.productDetails=product;
   }
-  public products=[
-    {
-    id: 1,
-    name: 'Product 1',
-    price:'100',
-    description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
-    imgURL: 'assets/images/home/img.png'
-    },
+ 
 
-    {
-      id: 2,
-      name: 'Product 2',
-      price:'200',
-      description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
-      imgURL: 'assets/images/home/img.png'
-    },
-
-    {
-      id: 3,
-      name: 'Product 3',
-      price:'300',
-      description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
-      imgURL: 'assets/images/home/img.png'
-    },
-
-    {
-      id: 4,
-      name: 'Product 4',
-      price:'400',
-      description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
-      imgURL: 'assets/images/home/img.png'
-    },
-
-    {
-      id: 5,
-      name: 'Product 5',
-      price:'500',
-      description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
-      imgURL: 'assets/images/home/img.png'
-    },
-
-      
-
-
-  ]
+  constructor( public apiService: ApiService){
+      this.apiService.getProducts().subscribe((productResponse) => {
+      console.log(productResponse);
+      this.products = productResponse.data;
+      });
+  }
 }
